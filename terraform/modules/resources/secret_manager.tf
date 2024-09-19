6d2172f87a576608a5a0090c6ae87d8bc8a0b9c6
YaelWil <yaelwilamo@gmail.com>
2024-09-19 22:00:45 +0300

    Changes to resources names + update git workflow yaml

--- terraform/modules/resources/secret_manager.tf
+++ terraform/modules/resources/secret_manager.tf
@@ -27,3 +27,16 @@ resource "aws_secretsmanager_secret_version" "certificate_version" {
   secret_id     = aws_secretsmanager_secret.certificate.id
   secret_string = tls_self_signed_cert.example.cert_pem
 }
+
+resource "aws_secretsmanager_secret" "private_key" {
+  name = "${var.owner}-certificate-PK-${var.env}-${var.region}-${uuid()}-${var.project}"
+
+  lifecycle {
+    ignore_changes = [name]
+  }
+}
+
+resource "aws_secretsmanager_secret_version" "private_key" {
+  secret_id     = aws_secretsmanager_secret.private_key.id
+  secret_string = tls_private_key.example.private_key_pem
+}
