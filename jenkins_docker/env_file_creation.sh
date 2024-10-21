e88c0544a95e844a4ec51cfc3af8105e8f89aac2
Yaelwil <143732803+Yaelwil@users.noreply.github.com>
2024-10-21 14:12:35 -0400

    Create env file

---
+++ jenkins_docker/env_file_creation.sh
@@ -0,0 +1,7 @@
+# Extract Jenkins secret and Docker group ID
+JENKINS_SECRET=$(sudo docker exec jenkins_docker-jenkins-1 cat /var/jenkins_home/secrets/initialAdminPassword)
+DOCKER_GROUP_ID=$(sudo getent group docker | cut -d ':' -f 3)
+
+# Create or overwrite the .env file with proper formatting
+echo "JENKINS_SECRET=$JENKINS_SECRET" > ./jenkins_docker/.env 
+echo "DOCKER_GROUP_ID=$DOCKER_GROUP_ID" >> ./jenkins_docker/.env
