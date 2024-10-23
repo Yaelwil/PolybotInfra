42eba5263c13f3c64d6b9b66e7f8cb755dc1495c
Yaelwil <143732803+Yaelwil@users.noreply.github.com>
2024-10-23 17:38:30 -0400

    Update jenkins_secret.sh

--- jenkins_docker/jenkins_secret.sh
+++ jenkins_docker/jenkins_secret.sh
@@ -1,5 +1,10 @@
-# Extract Jenkins secret and Docker group ID
-JENKINS_SECRET=$(sudo docker exec jenkins_docker-jenkins-1 cat /var/jenkins_home/secrets/initialAdminPassword)
+# Initialize the Jenkins secret variable
+JENKINS_SECRET=""
 
+# Wait for the Jenkins initialAdminPassword file to be available
+while [ -z "$JENKINS_SECRET" ]; do
+    JENKINS_SECRET=$(sudo docker exec jenkins_docker-jenkins-1 cat /var/jenkins_home/secrets/initialAdminPassword)
+    sleep 5  # Wait for 5 seconds before trying again
+done
 # Create or overwrite the .env file with proper formatting
-echo "JENKINS_SECRET=$JENKINS_SECRET" >> ./.env 
+echo "JENKINS_SECRET=$JENKINS_SECRET" >> ./.env
