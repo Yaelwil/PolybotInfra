a910b86c836c89d347f7de61295fb419c5dae25e
Yaelwil <143732803+Yaelwil@users.noreply.github.com>
2024-10-23 17:19:04 -0400

    Update env_file_creation.sh

--- jenkins_docker/env_file_creation.sh
+++ jenkins_docker/env_file_creation.sh
@@ -3,5 +3,5 @@ JENKINS_SECRET=$(sudo docker exec jenkins_docker-jenkins-1 cat /var/jenkins_home
 DOCKER_GROUP_ID=$(sudo getent group docker | cut -d ':' -f 3)
 
 # Create or overwrite the .env file with proper formatting
-echo "JENKINS_SECRET=$JENKINS_SECRET" > ./jenkins_docker/.env 
-echo "DOCKER_GROUP_ID=$DOCKER_GROUP_ID" >> ./jenkins_docker/.env
+echo "JENKINS_SECRET=$JENKINS_SECRET" > ./.env 
+echo "DOCKER_GROUP_ID=$DOCKER_GROUP_ID" >> ./.env
