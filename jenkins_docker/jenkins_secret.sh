5d57666a4e49db9e2d99965de915cdf9e04336af
Yaelwil <143732803+Yaelwil@users.noreply.github.com>
2024-10-23 17:21:18 -0400

    Update jenkins_secret.sh

--- jenkins_docker/jenkins_secret.sh
+++ jenkins_docker/jenkins_secret.sh
@@ -2,4 +2,4 @@
 JENKINS_SECRET=$(sudo docker exec jenkins_docker-jenkins-1 cat /var/jenkins_home/secrets/initialAdminPassword)
 
 # Create or overwrite the .env file with proper formatting
-echo "JENKINS_SECRET=$JENKINS_SECRET" >> ./jenkins_docker/.env 
+echo "JENKINS_SECRET=$JENKINS_SECRET" >> ./.env 
