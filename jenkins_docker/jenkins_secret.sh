54405bfcf7f88eef80309d5a2b9863ed38bc11dd
Yaelwil <143732803+Yaelwil@users.noreply.github.com>
2024-10-23 16:57:53 -0400

    Create jenkins_secret.sh

---
+++ jenkins_docker/jenkins_secret.sh
@@ -0,0 +1,5 @@
+# Extract Jenkins secret and Docker group ID
+JENKINS_SECRET=$(sudo docker exec jenkins_docker-jenkins-1 cat /var/jenkins_home/secrets/initialAdminPassword)
+
+# Create or overwrite the .env file with proper formatting
+echo "JENKINS_SECRET=$JENKINS_SECRET" >> ./jenkins_docker/.env 
